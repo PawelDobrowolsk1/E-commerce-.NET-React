e3d9191135416188ec650721873c7226be12c580
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-03-24 00:15:25 +0100

    Added exception handling middleware

---
+++ API/Middleware/ExceptionMiddlerawe.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddlerawe
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddlerawe> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddlerawe(RequestDelegate next, ILogger<ExceptionMiddlerawe> logger,
+            IHostEnvironment env)
+        {
+            _next= next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+
+                var response = new ProblemDetails()
+                {
+                    Status = 500,
+                    Detail = _env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
+                    Title = ex.Message,
+                };
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
