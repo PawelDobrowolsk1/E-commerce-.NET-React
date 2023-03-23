892e5133f0bb910bee2aa077ee5c0ad14905ec1e
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-03-23 14:15:39 +0100

    Added an AppBar

---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,15 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function Header () {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6">
+                    Re-Store
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
