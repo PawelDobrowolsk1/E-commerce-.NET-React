bcb1e37915dc893523234de16cbca10fea792e6d
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-04-04 19:08:46 +0200

    Created an orders controller

---
+++ API/DTOs/CreateOrderDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class CreateOrderDto
+    {
+        public bool SaveAddress { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+    }
+}
